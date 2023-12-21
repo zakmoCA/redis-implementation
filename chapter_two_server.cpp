85dc8365a50e8aba016ac86eed1595f7f10024c9
zakmoCA <zakeriyaamohamed@gmail.com>
2023-12-22 03:35:26 +1000

    server done, remove comments as more familiar

--- chapter_two_server.cpp
+++ chapter_two_server.cpp
@@ -9,15 +9,15 @@
 
 // introduction to sockets
 
-static void do_something(int connfd) {
-  char rbuf[64] = {};
-  ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+static void handleClient(int connfd) {
+  char rbuf[64] = {}; // this is a buffer to store client msg
+  ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1); // read data from client into buffer
   if (n < 0) {
     msg("read() error");
   }
   printf("client says: %s\n", rbuf);
 
-  char wbuf[] = "world";
+  char wbuf[] = "world"; // message to retun client
   write(connfd, wbuf, strlen(wbuf));
 }
 
@@ -27,8 +27,10 @@ void msg(const char *message) {
   exit(EXIT_FAILURE); // exits program
 }
 
+// repeating msg() but better for clarity for now
 void die(const char *message) {
-
+  perror(message);
+  exit(EXIT_FAILURE);
 }
 
 int main() {
@@ -42,28 +44,29 @@ int main() {
   addr.sin_family = AF_INET;
   addr.sin_port = htons(1234);  // make sure to use network byte order
   addr.sin_addr.s_addr = htonl(INADDR_ANY);  // for binding to 0.0.0.0 wildcard address
-  int rv = bind(fd, (struct sockaddr *)&addr, sizeof(addr));
+  int rv = bind(fd, (struct sockaddr *)&addr, sizeof(addr)); // bind socket to the specified IP and port
   if (rv) {
-    die("bind()");
+    die("bind() failed");
   }
 
   // listen
-  rv = listen(fd, SOMAXCONN);
+  rv = listen(fd, SOMAXCONN); // listen on the socket for incoming connections
+  // SOMAXCONN defines max length for the queue of pending connections https://stackoverflow.com/questions/18073483/what-do-somaxconn-mean-in-c-socket-programming
   if (rv) {
-    die("listen()");
+    die("listen() failed");
   }
 
   while (true) {
-    // accept
-    struct sockaddr_in client_addr = {};
+    // continuously accept new connections
+    struct sockaddr_in client_addr = {}; // the client addr which will be filled in by accept()
     socklen_t socklen = sizeof(client_addr);
-    int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+    int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen); //accept new connection
     if (connfd < 0) {
-      continue;   // error
+      continue;   // skip rest of loop if accept fails
     }
 
-    do_something(connfd);
-    close(connfd);
+    handleClient(connfd);
+    close(connfd); // after handling
   }
       
   return 0;
